45d5c9300eaf3d1f1c9cf387456ceb06a407405e
Danscot <99121218+Danscot@users.noreply.github.com>
2025-06-23 02:24:59 +0100

    Update index.d.ts

--- lib/Defaults/index.d.ts
+++ lib/Defaults/index.d.ts
@@ -1,51 +1,113 @@
-import { proto } from '../../WAProto';
-import type { MediaType, SocketConfig } from '../Types';
-export declare const UNAUTHORIZED_CODES: number[];
-export declare const DEFAULT_ORIGIN = "https://web.whatsapp.com";
-export declare const DEF_CALLBACK_PREFIX = "CB:";
-export declare const DEF_TAG_PREFIX = "TAG:";
-export declare const PHONE_CONNECTION_CB = "CB:Pong";
-export declare const WA_DEFAULT_EPHEMERAL: number;
-export declare const NOISE_MODE = "Noise_XX_25519_AESGCM_SHA256\0\0\0\0";
-export declare const DICT_VERSION = 2;
-export declare const KEY_BUNDLE_TYPE: Buffer<ArrayBuffer>;
-export declare const NOISE_WA_HEADER: Buffer<ArrayBuffer>;
+import { proto } from '../../WAProto'
+import { makeLibSignalRepository } from '../Signal/libsignal'
+import type { AuthenticationState, MediaType, SocketConfig, WAVersion } from '../Types'
+import { Browsers } from '../Utils'
+import logger from '../Utils/logger'
+import { version } from './baileys-version.json'
+
+export const UNAUTHORIZED_CODES = [401, 403, 419]
+
+export const DEFAULT_ORIGIN = 'https://web.whatsapp.com'
+export const DEF_CALLBACK_PREFIX = 'CB:'
+export const DEF_TAG_PREFIX = 'TAG:'
+export const PHONE_CONNECTION_CB = 'CB:Pong'
+
+export const WA_DEFAULT_EPHEMERAL = 7 * 24 * 60 * 60
+
+export const NOISE_MODE = 'Noise_XX_25519_AESGCM_SHA256\0\0\0\0'
+export const DICT_VERSION = 3
+export const KEY_BUNDLE_TYPE = Buffer.from([5])
+export const NOISE_WA_HEADER = Buffer.from([87, 65, 6, DICT_VERSION]) // last is "DICT_VERSION"
 /** from: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url */
-export declare const URL_REGEX: RegExp;
-export declare const WA_CERT_DETAILS: {
-    SERIAL: number;
-};
-export declare const PROCESSABLE_HISTORY_TYPES: proto.Message.HistorySyncNotification.HistorySyncType[];
-export declare const DEFAULT_CONNECTION_CONFIG: SocketConfig;
-export declare const MEDIA_PATH_MAP: {
-    [T in MediaType]?: string;
-};
-export declare const MEDIA_HKDF_KEY_MAPPING: {
-    audio: string;
-    document: string;
-    gif: string;
-    image: string;
-    ppic: string;
-    product: string;
-    ptt: string;
-    sticker: string;
-    video: string;
-    'thumbnail-document': string;
-    'thumbnail-image': string;
-    'thumbnail-video': string;
-    'thumbnail-link': string;
-    'md-msg-hist': string;
-    'md-app-state': string;
-    'product-catalog-image': string;
-    'payment-bg-image': string;
-    ptv: string;
-};
-export declare const MEDIA_KEYS: MediaType[];
-export declare const MIN_PREKEY_COUNT = 5;
-export declare const INITIAL_PREKEY_COUNT = 30;
-export declare const DEFAULT_CACHE_TTLS: {
-    SIGNAL_STORE: number;
-    MSG_RETRY: number;
-    CALL_OFFER: number;
-    USER_DEVICES: number;
-};
+export const URL_REGEX = /https:\/\/(?![^:@\/\s]+:[^:@\/\s]+@)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?/g
+
+export const WA_CERT_DETAILS = {
+	SERIAL: 0
+}
+
+export const PROCESSABLE_HISTORY_TYPES = [
+	proto.Message.HistorySyncNotification.HistorySyncType.INITIAL_BOOTSTRAP,
+	proto.Message.HistorySyncNotification.HistorySyncType.PUSH_NAME,
+	proto.Message.HistorySyncNotification.HistorySyncType.RECENT,
+	proto.Message.HistorySyncNotification.HistorySyncType.FULL,
+	proto.Message.HistorySyncNotification.HistorySyncType.ON_DEMAND
+]
+
+export const DEFAULT_CONNECTION_CONFIG: SocketConfig = {
+	version: version as WAVersion,
+	browser: Browsers.ubuntu('Chrome'),
+	waWebSocketUrl: 'wss://web.whatsapp.com/ws/chat',
+	connectTimeoutMs: 20_000,
+	keepAliveIntervalMs: 30_000,
+	logger: logger.child({ class: 'baileys' }),
+	emitOwnEvents: true,
+	defaultQueryTimeoutMs: 60_000,
+	customUploadHosts: [],
+	retryRequestDelayMs: 250,
+	maxMsgRetryCount: 5,
+	fireInitQueries: true,
+	auth: undefined as unknown as AuthenticationState,
+	markOnlineOnConnect: true,
+	syncFullHistory: false,
+	patchMessageBeforeSending: msg => msg,
+	shouldSyncHistoryMessage: () => true,
+	shouldIgnoreJid: () => false,
+	linkPreviewImageThumbnailWidth: 192,
+	transactionOpts: { maxCommitRetries: 10, delayBetweenTriesMs: 3000 },
+	generateHighQualityLinkPreview: false,
+	options: {},
+	appStateMacVerification: {
+		patch: false,
+		snapshot: false
+	},
+	countryCode: 'US',
+	getMessage: async () => undefined,
+	cachedGroupMetadata: async () => undefined,
+	makeSignalRepository: makeLibSignalRepository
+}
+
+export const MEDIA_PATH_MAP: { [T in MediaType]?: string } = {
+	image: '/mms/image',
+	video: '/mms/video',
+	document: '/mms/document',
+	audio: '/mms/audio',
+	sticker: '/mms/image',
+	'thumbnail-link': '/mms/image',
+	'product-catalog-image': '/product/image',
+	'md-app-state': '',
+	'md-msg-hist': '/mms/md-app-state'
+}
+
+export const MEDIA_HKDF_KEY_MAPPING = {
+	audio: 'Audio',
+	document: 'Document',
+	gif: 'Video',
+	image: 'Image',
+	ppic: '',
+	product: 'Image',
+	ptt: 'Audio',
+	sticker: 'Image',
+	video: 'Video',
+	'thumbnail-document': 'Document Thumbnail',
+	'thumbnail-image': 'Image Thumbnail',
+	'thumbnail-video': 'Video Thumbnail',
+	'thumbnail-link': 'Link Thumbnail',
+	'md-msg-hist': 'History',
+	'md-app-state': 'App State',
+	'product-catalog-image': '',
+	'payment-bg-image': 'Payment Background',
+	ptv: 'Video'
+}
+
+export const MEDIA_KEYS = Object.keys(MEDIA_PATH_MAP) as MediaType[]
+
+export const MIN_PREKEY_COUNT = 5
+
+export const INITIAL_PREKEY_COUNT = 30
+
+export const DEFAULT_CACHE_TTLS = {
+	SIGNAL_STORE: 5 * 60, // 5 minutes
+	MSG_RETRY: 60 * 60, // 1 hour
+	CALL_OFFER: 5 * 60, // 5 minutes
+	USER_DEVICES: 5 * 60 // 5 minutes
+}
